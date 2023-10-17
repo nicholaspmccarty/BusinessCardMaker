d405294d832c6ff9e35b7c1ecf051c91ed675323
mccartnp <nicholasmccarty252@gmail.com>
2023-10-17 15:59:49 -0400

    Installed ncss into the node package manager

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function App() {
-  return <div className="app">Hello</div>;
+const App = () => {
+  return <div clasName="hello"> hello
+  <header>
+    <h1><a href="">Logo</a></h1>
+    <nav>
+      <ul>
+        <li><a href="">Link 1</a></li>
+        <li><a href="">Link 2</a></li>
+        <li><a href=""> Link 3</a></li>
+      </ul>
+
+    </nav>
+  </header>
+  </div>
 }
 
 export default App;
