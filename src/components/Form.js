8cd23fbed9b2118bd1a3e9dac8e4ffb533836830
mccartnp <nicholasmccarty252@gmail.com>
2023-10-17 17:27:48 -0400

    Forms finished

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import FormElement from './FormElement';
+
 
 const Form = () => {
+  
+
   return (
-   <section className="Form">Form</section>
-  )
-}
+     <>
+      <form>
+      <h2> Enter your details</h2>
+      <label style={{ display: 'block', marginBottom: '5px' }}>
+          <strong>Name</strong>
+          <input type="text" style={{ display: 'block',  marginBottom: '15px'}} />
+        </label>
+        <label style={{ display: 'block', marginBottom: '5px' }}>
+          <strong>Designation</strong>
+          <input type="text" style={{ display: 'block', marginBottom: '15px' }} />
+        </label>
+        <label style={{ display: 'block', marginBottom: '5px' }}>
+          <strong>Phone</strong>
+          <input type="text" style={{ display: 'block', marginBottom: '15px'}} />
+        </label>
+      </form>
+    </>
+    
+    
+  );
+  
+};
 
 export default Form
